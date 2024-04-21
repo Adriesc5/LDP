9061da4050a255649daed9a0a99763402cfa5c0f
Francicso Escobedo <Adriesc57@gmail.com>
2024-04-21 11:38:50 -0600

    Se realiza el primer ejercicio de mouredev

---
+++ 00_Sintaxis_var_hellow.py
@@ -0,0 +1,18 @@
+# https://www.python.org/
+"""
+Esto es un comentario en varias lineas
+"""
+my_variable = "Mi variable"
+my_variable ="Nuevo valor de mi variable/**/"
+MY_CONSTAT = "Por convencion ponemos en mayusculas las variables que no queremos cambiar"
+#En python existen 4 tipos de valores principales
+my_int = 1 #Numeros de datos enteros
+my_float = 1.5 #Numeros con decimales
+my_bool = False #Solo se puede verdadero o falso, 1 o 0
+my_string = "Cadena de texto"
+print("Hello,Python")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
